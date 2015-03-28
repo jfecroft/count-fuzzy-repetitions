03adcf68402997e616ceaf1757697a7684e14879
jfecroft <j.f.e.croft@gmail.com>
2015-03-28 21:32:48 +0000

    use consistant naming

--- parse_text.py
+++ parse_text.py
@@ -17,7 +17,8 @@ ARGUMENTS = docopt(__doc__, version='parse text 1.0')
 INPUT_DICT = load_yaml(ARGUMENTS['<input>'])
 BOOKS = INPUT_DICT['books']
 
-luc = CountRepititions(BOOKS, npairs=INPUT_DICT['n'], strip=INPUT_DICT['strip'])
+#luc = CountRepititions(BOOKS, npairs=INPUT_DICT['n'], strip=INPUT_DICT['strip'])
+luc = CountRepititions(**INPUT_DICT)
 luc.get_identical_words()
-luc.write_repititions(filen='repititions_{}pairs.dat'.format(INPUT_DICT['n']),
+luc.write_repititions(filen='repititions_{}pairs.dat'.format(INPUT_DICT['npairs']),
                       min_reps=INPUT_DICT['min_reps'])
