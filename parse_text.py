fef524ec013d210619bbf7c03aac958bbaaa4dcc
jfecroft <j.f.e.croft@gmail.com>
2015-03-28 21:48:50 +0000

    [cl] pylint

--- parse_text.py
+++ parse_text.py
@@ -15,9 +15,9 @@ from rep_funcs import (
 
 ARGUMENTS = docopt(__doc__, version='parse text 1.0')
 INPUT_DICT = load_yaml(ARGUMENTS['<input>'])
-BOOKS = INPUT_DICT['books']
 
-luc = CountRepetitions(**INPUT_DICT)
-luc.get_identical_words()
-luc.write_repetitions(filen='repetitions_{}pairs.dat'.format(INPUT_DICT['npairs']),
-                      min_reps=INPUT_DICT['min_reps'])
+LUC = CountRepetitions(**INPUT_DICT)
+LUC.get_identical_words()
+LUC.write_repetitions(
+    filen='repetitions_{}pairs.dat'.format(INPUT_DICT['npairs']),
+    min_reps=INPUT_DICT['min_reps'])
