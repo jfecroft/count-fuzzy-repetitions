9fe7c1df3600ab24c60957eef72b4e64c44058d5
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 23:55:58 +0100

    [cl] pylint

--- lucretius-de_rerum_natura-xml2txt.py
+++ lucretius-de_rerum_natura-xml2txt.py
@@ -1,3 +1,4 @@
+# pylint: disable=C0103
 """
 convert lucretius xml to seperate text file for each book
 """
@@ -10,16 +11,15 @@ BOOKS = ROOT.findall('.//*div1[@type="book"]')
 for i, book in enumerate(BOOKS):
     book_text = ''
     for line in book.iter():
-        if line.tag in ['lb', 'del', 'add'] and line.attrib.get('n', '').endswith('a') is False:
-            if line.tag == 'lb' and book_text.endswith('\n') is False and book_text:
+        if (line.tag in ['lb', 'del', 'add'] and
+                line.attrib.get('n', '').endswith('a') is False):
+            if (line.tag == 'lb' and book_text.endswith('\n') is False and
+                    book_text):
                 book_text += '\n'
             if line.text:
                 book_text += line.text.rstrip('\n').encode('utf8')
             if line.tail:
                 book_text += line.tail.rstrip('\n').encode('utf8')
-          #  if line.attrib.get('n' , '').endswith('a'):
-          #      import pdb
-          #      pdb.set_trace()
 
     with open('{}-book{}.txt'.format(FILEN, i+1), 'w') as ofile:
         ofile.write(book_text)
