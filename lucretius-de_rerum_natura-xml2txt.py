0e68432e0c9fba2e83405c30c4540094c33cb2ef
jfecroft <j.f.e.croft@gmail.com>
2015-04-14 23:42:07 +0100

    [cl] pylint

--- lucretius-de_rerum_natura-xml2txt.py
+++ lucretius-de_rerum_natura-xml2txt.py
@@ -1,13 +1,13 @@
 import xml.etree.ElementTree as ET
-filen = 'lucretius-de_rerum_natura'
-tree = ET.parse('{}.xml'.format(filen))
-root = tree.getroot()
+FILEN = 'lucretius-de_rerum_natura'
+TREE = ET.parse('{}.xml'.format(FILEN))
+ROOT = TREE.getroot()
 
-books = root.findall('.//*div1[@type="book"]')
-for i, book in enumerate(books):
+BOOKS = ROOT.findall('.//*div1[@type="book"]')
+for i, book in enumerate(BOOKS):
     book_text = ''
     for line in book.iter():
-        if line.tag in ['lb', 'del', 'add'] and line.attrib.get('n' , '').endswith('a') is False:
+        if line.tag in ['lb', 'del', 'add'] and line.attrib.get('n', '').endswith('a') is False:
             if line.tag == 'lb' and book_text.endswith('\n') is False and book_text:
                 book_text += '\n'
             if line.text:
@@ -18,5 +18,5 @@ for i, book in enumerate(books):
           #      import pdb
           #      pdb.set_trace()
 
-    with open('{}-book{}.txt'.format(filen, i+1), 'w') as ofile:
+    with open('{}-book{}.txt'.format(FILEN, i+1), 'w') as ofile:
         ofile.write(book_text)
